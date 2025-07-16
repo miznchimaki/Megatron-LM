51111231eede24c15b7227477b86b20be20325cc
miznchimaki <miznchimaki@gmail.com>
2025-07-16 15:06:12 +0800

    small fix

--- examples/mixtral/mixtral_data_preprocessing.sh
+++ examples/mixtral/mixtral_data_preprocessing.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
 
 
-INPUT_DATA_FILE=${1:-"${HOME}/datasetse/megatron-lm-data/seq_monkey_datawhale.jsonl"}
+INPUT_DATA_FILE=${1:-"${HOME}/datasets/megatron-lm-data/seq_monkey_datawhale.jsonl"}
 OUTPUT_PREFIX=${2:-"mixtral-pretrain"}
 TOKENIZER_TYPE=${3:-"Llama2Tokenizer"}
-TOKENIZER_MODEL=${4:-"{HOME}/ckpts/Mixtral-8x7B-Instruct-v0.1/tokenizer.model"}
-JOSN_KEY=${5:-"text"}
-LOG_FILE=${6:-"mixtral_data_preprocessing.log"}
+TOKENIZER_MODEL=${4:-"${HOME}/ckpts/Mixtral-8x7B-Instruct-v0.1/tokenizer.model"}
+JSON_KEYS=${5:-"text"}
+WORKERS=${6:-"223"}
+LOG_INTERVAL=${7:-"100"}
+LOG_FILE=${8:-"mixtral_data_preprocessing.log"}
 
 cd ${HOME}/projects/Megatron-LM/
 LOG_PATH=$(pwd)/examples/mixtral/${LOG_FILE}
@@ -17,6 +19,8 @@ python ./tools/preprocess_data.py \
     --output-prefix ${OUTPUT_PREFIX} \
     --tokenizer-type ${TOKENIZER_TYPE} \
     --tokenizer-model ${TOKENIZER_MODEL} \
-    --json-key ${JSON_KEY} 2>&1 | tee --append ${LOG_PATH}
+    --workers ${WORKERS} \
+    --log-interval ${LOG_INTERVAL} \
+    --json-keys ${JSON_KEYS} 2>&1 | tee --append ${LOG_PATH}
 
 echo "end mixtral training data preprocessing at `date '+%Y-%m-%d-%H:%M:%S'`" 2>&1 | tee --append ${LOG_PATH}
