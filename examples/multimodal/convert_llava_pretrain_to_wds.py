9cdb1086a208b813291da6aa3ab0d4c12ea80cb4
miznchimaki <miznchimaki@gmail.com>
2025-08-21 21:17:02 +0800

    small update for MLLM dataset convertion

--- examples/multimodal/convert_llava_pretrain_to_wds.py
+++ examples/multimodal/convert_llava_pretrain_to_wds.py
@@ -1,10 +1,17 @@
 import json
-import os
+import os, sys
+import warnings
 import webdataset as wds
-
 from tqdm import tqdm
 
-llava_pretrain_dir = '<path_to_LLaVA-Pretrain>'
+
+try:
+    llava_pretrain_dir = sys.argv[1]
+except IndexError as _:
+    warnings.warn("please pass in an argument for LLaVA-Pretrain dataset")
+
+if not os.path.exists(llava_pretrain_dir):
+     raise FileNotFoundError(f"the passed in param for LLaVA-Pretrain dataset - {llava_pretrain_dir}, does not exist.")
 
 # Paths to the dataset files
 json_file = os.path.join(llava_pretrain_dir, 'blip_laion_cc_sbu_558k.json')
@@ -19,7 +26,7 @@ with open(json_file, 'r') as f:
 
 with wds.ShardWriter(os.path.join(output, 'pretrain-%d.tar'), maxcount=10000) as shard_writer:
     for entry in tqdm(data):
-        with open(os.path.join(llava_pretrain_dir, entry['image']), "rb") as img_file:
+        with open(os.path.join(llava_pretrain_dir, 'images', entry['image']), "rb") as img_file:
                 image_data = img_file.read()
         sample = {
             "__key__": entry['id'],
